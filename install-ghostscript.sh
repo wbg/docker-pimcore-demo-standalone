362faf49b4ceef220dd336522945217f4a6b5211
Bernhard Rusch <bernhard.rusch@gmail.com>
2015-02-02 21:57:34 +0100

    Update install-ghostscript.sh

--- install-ghostscript.sh
+++ install-ghostscript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+apt-get -y build-dep ghostscript
+
 wget http://downloads.ghostscript.com/public/ghostscript-9.10.tar.gz -O ghostscript.tgz
 tar xfvz ghostscript.tgz
 cd ghostscript-*/
