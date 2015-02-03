3438da6f2df0492fc9b48975db6859eccbd63af9
Bernhard Rusch <bernhard.rusch@gmail.com>
2015-02-03 08:11:24 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+# install pimcore if needed
+if [ ! -d /var/www/pimcore ]; then
+  # download & extract
+  cd /var/www
+  wget https://www.pimcore.org/download/pimcore-latest.zip -O pimcore.zip 
+  unzip pimcore.zip && rm pimcore.zip 
+  
+  # setup database 
+  # ... 
+fi
+
 exec supervisord -n
