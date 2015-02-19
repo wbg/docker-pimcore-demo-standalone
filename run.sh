2d86e6b33484a7ece76399e594caf8314c904395
Bernhard Rusch <bernhard.rusch@gmail.com>
2015-02-19 09:33:04 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,22 +7,25 @@
 if [ ! -d /var/www/pimcore ]; then
   # download & extract
   cd /var/www
-  sudo -u www-data wget https://www.pimcore.org/download/pimcore-latest.zip -O pimcore.zip 
-  sudo -u www-data unzip pimcore.zip && rm pimcore.zip 
+  rm -r /var/www/*
+  sudo -u www-data wget https://www.pimcore.org/download/pimcore-latest.zip -O /tmp/pimcore.zip 
+  sudo -u www-data unzip pimcore.zip -d /var/www/
+  rm /tmp/pimcore.zip 
   
   # create demo mysql user
   mysql -u root -e "CREATE USER 'pimcore_demo'@'%' IDENTIFIED BY 'secretpassword';"
   mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'pimcore_demo'@'%' WITH GRANT OPTION;"
   
   # setup database 
-  mysql -u pimcore_demo -e "CREATE DATABASE pimcore_demo_pimcore charset=utf8;"; 
-  mysql -u pimcore_demo pimcore_demo_pimcore < ./website/dump/data.sql
+  mysql -u pimcore_demo -psecretpassword -e "CREATE DATABASE pimcore_demo_pimcore charset=utf8;"; 
+  mysql -u pimcore_demo -psecretpassword pimcore_demo_pimcore < ./website/dump/data.sql
   
   # 'admin' password is 'demo' 
-  mysql -u pimcore_demo -D pimcore_demo_pimcore -e "UPDATE users SET password = '\$2y\$10\$3dykbihqPig8UseacT1AgucP2IdRPcmA56wVyFVgIw1RSjCOvmfhm' WHERE name = 'admin'"  
-  mysql -u pimcore_demo -D pimcore_demo_pimcore -e "UPDATE users SET id = '0' WHERE name = 'system'"
+  mysql -u pimcore_demo -psecretpassword -D pimcore_demo_pimcore -e "UPDATE users SET password = '\$2y\$10\$3dykbihqPig8UseacT1AgucP2IdRPcmA56wVyFVgIw1RSjCOvmfhm' WHERE name = 'admin'"  
+  mysql -u pimcore_demo -psecretpassword -D pimcore_demo_pimcore -e "UPDATE users SET id = '0' WHERE name = 'system'"
   
-  sudo -u www-data mv ./website/var/config/system.xml.template ./website/var/config/system.xml
+  sudo -u www-data mv /var/www/website/var/config/system.xml.template /var/www/website/var/config/system.xml
+  sudo -u www-data cp /tmp/cache.xml /var/www/website/var/config/cache.xml
 fi
 
 # stop temp. mysql service
