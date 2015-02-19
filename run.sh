db05711159564b15f19cfab4de49c818b0adb39b
Bernhard Rusch <bernhard.rusch@gmail.com>
2015-02-19 09:26:34 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,14 +3,12 @@
 # temp. start mysql to do all the install stuff
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
-su www-data
-
 # install pimcore if needed
 if [ ! -d /var/www/pimcore ]; then
   # download & extract
   cd /var/www
-  wget https://www.pimcore.org/download/pimcore-latest.zip -O pimcore.zip 
-  unzip pimcore.zip && rm pimcore.zip 
+  sudo -u www-data wget https://www.pimcore.org/download/pimcore-latest.zip -O pimcore.zip 
+  sudo -u www-data unzip pimcore.zip && rm pimcore.zip 
   
   # create demo mysql user
   mysql -u root -e "CREATE USER 'pimcore_demo'@'%' IDENTIFIED BY 'secretpassword';"
@@ -24,11 +22,8 @@ if [ ! -d /var/www/pimcore ]; then
   mysql -u pimcore_demo -D pimcore_demo_pimcore -e "UPDATE users SET password = '\$2y\$10\$3dykbihqPig8UseacT1AgucP2IdRPcmA56wVyFVgIw1RSjCOvmfhm' WHERE name = 'admin'"  
   mysql -u pimcore_demo -D pimcore_demo_pimcore -e "UPDATE users SET id = '0' WHERE name = 'system'"
   
-  mv ./website/var/config/system.xml.template ./website/var/config/system.xml
+  sudo -u www-data mv ./website/var/config/system.xml.template ./website/var/config/system.xml
 fi
-
-# go back to root, to start the daemons
-exit; 
 
 # stop temp. mysql service
 mysqladmin -uroot shutdown
